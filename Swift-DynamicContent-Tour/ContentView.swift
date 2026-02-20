4e35ff881effce4d437a4fe402383c62c124f4cb
elias <eliaschen.dev@gmail.com>
2026-02-21 00:57:05 +0800

    finish learning demo

--- Swift-DynamicContent-Tour/ContentView.swift
+++ Swift-DynamicContent-Tour/ContentView.swift
@@ -8,12 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names: [String] = [
+        "Elias", "John", "Ian", "Sammi",
+    ]
+    @State private var inputName = ""
+    @State private var pickName = ""
+    @State private var shouldRemoveAfterPick: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "person.3.sequence.fill").font(.largeTitle)
+                .foregroundStyle(.tint).symbolRenderingMode(.hierarchical)
+            Text("Pick-A-Pal").font(.title).fontWeight(.bold)
+            Text(pickName.isEmpty ? " " : pickName).font(.title3).fontWeight(
+                .bold
+            ).foregroundStyle(.tint)
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }.clipShape(RoundedRectangle(cornerRadius: 20))
+            TextField("Add Name", text: $inputName).onSubmit {
+                if !inputName.isEmpty {
+                    names.append(inputName)
+                    inputName = ""
+                }
+            }
+            Divider()
+            Toggle("Remove after pick", isOn: $shouldRemoveAfterPick)
+            Button {
+                if let randomName = names.randomElement() {
+                    pickName = randomName
+                    if shouldRemoveAfterPick {
+                        names.removeAll { $0 == randomName }
+                    }
+                } else {
+                    pickName = ""
+                }
+            } label: {
+                Text("Pick a random name").fontWeight(.medium).padding(8)
+            }.buttonStyle(.borderedProminent).font(.title3)
         }
         .padding()
     }
