6465029772471c32e85f2501226ca5c6c5c17181
elias <eliaschen.dev@gmail.com>
2026-02-21 00:23:29 +0800

    Initial Commit

---
+++ Swift-DynamicContent-Tour/Swift_DynamicContent_TourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DynamicContent_TourApp.swift
+//  Swift-DynamicContent-Tour
+//
+//  Created by Elias on 2026/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DynamicContent_TourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
